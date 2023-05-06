2941f4608bd4243d5c71f8079d007e5d2ede4c63
rifflesss <roamchevif@gmail.com>
2023-05-06 17:59:28 +0300

    -My Events and Activities templates added

--- course/events/urls.py
+++ course/events/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.student),
     path('events/', views.events),
+    path('activities/', views.activities),
+    path('myevents/', views.myevents),
 ]
--- course/events/views.py
+++ course/events/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 def student(request):
-    return render(request, 'events/student.html')
+    events = Event.objects.all()
+    return render(request, 'events/student.html', {'events': events})
 
 def events(request):
     return render(request, 'events/events.html')
 
-#def activity(request):
-#   return HttpResponse('Event page')
+def activities(request):
+    activities = Activity.objects.first()
+    spots_left = activities.spots - activities.spots_taken
+    return render(request, 'events/activities.html', {'activities': activities, 'spots_left': spots_left})
+
+def myevents(request):
+    events = Event.objects.all()
+    return render(request, 'events/student.html', {'events': events})
